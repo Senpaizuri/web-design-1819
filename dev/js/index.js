0a6a870f90d71c3c8d2e7f757cc91504a38b146a
Maikel Sleebos <mjsleebos@gmail.com>
2019-05-24 00:21:47 +0200

    Add inter-face. ha. joke.

--- dev/js/index.js
+++ dev/js/index.js
@@ -7,15 +7,33 @@
             treshold:.01
         },
         wordBlocks = Array.from(storyBlocks).forEach(word=> word.innerHTML.split(""))
-        console.log(storyBlocks)
     let 
         enhanceStory = (entries)=>{
             entries.forEach(entry=>{
                 const prevRatio = entry.target.getAttribute("data-ratio")
                 if(entry.intersectionRatio > prevRatio){
                     entry.target.classList.add("active")
+                    document.querySelector(`#people .${entry.target.classList[0]} .brow`).classList.add(entry.target.getAttribute("data-mood"))
+                    document.querySelector(`#people .ira .face`).classList.remove("mark","elna","ira")
+                    document.querySelector(`#people .mark .face`).classList.remove("mark","elna","ira")
+                    document.querySelector(`#people .elna .face`).classList.remove("mark","elna","ira")
+                    switch(entry.target.nextElementSibling.classList[0]){
+                        case "ira":
+                            document.querySelector(`#people .mark .face`).classList.add("ira")
+                            document.querySelector(`#people .elna .face`).classList.add("ira")
+                        break;
+                        case "elna":
+                            document.querySelector(`#people .mark .face`).classList.add("elna")
+                            document.querySelector(`#people .ira .face`).classList.add("elna")
+                        break;
+                        case "mark":
+                            document.querySelector(`#people .ira .face`).classList.add("mark")
+                            document.querySelector(`#people .elna .face`).classList.add("mark")
+                        break;
+                    }
                 }else{
                     entry.target.classList.remove("active")
+                    document.querySelector(`#people .${entry.target.classList[0]} .brow`).classList.remove("suprise","worried","mad","question")
                 }
                 entry.target.setAttribute("data-ratio",entry.intersectionRatio)
             })
